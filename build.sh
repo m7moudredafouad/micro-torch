a079fd2bcc88c7d78284c0aaaf7f012e803ff791
m7moudredafouad <m7moudredafouad@gmail.com>
2024-10-05 23:04:38 +0300

    fix building and support MSYS makefiles

--- build.sh
+++ build.sh
@@ -3,18 +3,29 @@ output_dir=build
 
 mkdir -p $output_dir
 
-cmake -S . -B $output_dir -DCMAKE_BUILD_TYPE=Debug
-if [ $? != 0 ]; then
-    echo ">>> Cmake error $? couldn't build the project"
-    exit $?
+if [[ "$OSTYPE" == "msys" ]]; then
+    cmake -S . -B $output_dir -DCMAKE_BUILD_TYPE=Debug -G "MSYS Makefiles"
+else
+    cmake -S . -B $output_dir -DCMAKE_BUILD_TYPE=Debug
+fi
+
+error=$?
+if [ $error != 0 ]; then
+    echo ">>> Cmake error $error couldn't build the project"
+    exit $error
 fi
 
 echo ">>> Building..."
 cd $output_dir
 make
-if [ $? == 0 ]; then
+error=$?
+if [ $error == 0 ]; then
     echo ">>> Done."
 else
-    echo ">>> Make error $?"
+    echo ">>> Make error $error"
 fi
 cd -
+
+if [[ "$OSTYPE" == "msys" ]]; then
+    ln -s $(pwd)/$output_dir/libs/glog/libglogd.dll $(pwd)/$output_dir
+fi
