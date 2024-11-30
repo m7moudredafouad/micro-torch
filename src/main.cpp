006eec7cc1367fdd596f9a3b379cc9dd201a566b
m7moudredafouad <m7moudredafouad@gmail.com>
2024-11-30 13:30:18 +0200

    add basic unit tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,6 @@
 #include "tensor.hpp"
 
 int main() {
-    // Tensor t1({3, 3}), t2({1, 3});
-    // t1 = 1;
-    // t1 *= 16.1;
-    // t1 /= 4;
-    // t1 -= 1;
-    // LOG(INFO) << t1;
-
-    // t2 = 2;
-    // t2[{0, 1}] = 0;
-    // LOG(INFO) << t2;
-
-    // auto t3 = t1 * t2;
-    // LOG(INFO) << t3;
-
-    // auto t4 = t1 + t3;
-    // LOG(INFO) << t4;
-
-    // Tensor tx({3, 6});
-    // tx = 1;
-    // t1 = 2;
-    // auto t5 = t1.mm(tx);
-    // LOG(INFO) << t5;
-
-    // Tensor ty({3}), tz({3});
-    // ty = 1;
-    // tz = 2.1;
-    // auto t6 = ty.mm(tz);
-    // LOG(INFO) << t6;
-
     Tensor t6({3}), t7({3}), t8({3});
     t6.requires_grad(true);
 
--- tests/unit/basic_operation.cpp
+++ tests/unit/basic_operation.cpp
@@ -15,7 +15,7 @@ TEST(BasicTensorOperations, Assignment) {
     }
 }
 
-TEST(BasicTensorOperations, Add) {
+TEST(BasicTensorOperations, AddConstant) {
     uint32_t tensor_size = 3;
     Tensor t1({tensor_size}, Tensor::Type::UINT32);
     t1 = 0;
@@ -26,5 +26,111 @@ TEST(BasicTensorOperations, Add) {
     for (uint32_t i = 0; i < tensor_size; i++) {
         index[0] = i;
         EXPECT_EQ((uint32_t)t1[index], 16);
+    }
+}
+
+TEST(BasicTensorOperations, SubConstant) {
+    uint32_t tensor_size = 3;
+    Tensor t1({tensor_size}, Tensor::Type::UINT32);
+    t1 = 16;
+    t1 -= 1;
+
+    std::vector<uint32_t> index(1, 0);
+
+    for (uint32_t i = 0; i < tensor_size; i++) {
+        index[0] = i;
+        EXPECT_EQ((uint32_t)t1[index], 15);
+    }
+}
+
+TEST(BasicTensorOperations, MulConstant) {
+    uint32_t tensor_size = 3;
+    Tensor t1({tensor_size}, Tensor::Type::UINT32);
+    t1 = 1;
+    t1 *= 16;
+
+    std::vector<uint32_t> index(1, 0);
+
+    for (uint32_t i = 0; i < tensor_size; i++) {
+        index[0] = i;
+        EXPECT_EQ((uint32_t)t1[index], 16);
+    }
+}
+
+TEST(BasicTensorOperations, DivConstant) {
+    uint32_t tensor_size = 3;
+    Tensor t1({tensor_size}, Tensor::Type::UINT32);
+    t1 = 16;
+    t1 /= 4;
+
+    std::vector<uint32_t> index(1, 0);
+
+    for (uint32_t i = 0; i < tensor_size; i++) {
+        index[0] = i;
+        EXPECT_EQ((uint32_t)t1[index], 4);
+    }
+}
+
+TEST(BasicTensorOperations, AddTensor) {
+    uint32_t tensor_size = 3;
+    Tensor t1({tensor_size}, Tensor::Type::UINT32);
+    Tensor t2({tensor_size}, Tensor::Type::UINT32);
+    t1 = 1;
+    t2 = 2;
+
+    auto t3 = t1 + t2;
+    std::vector<uint32_t> index(1, 0);
+
+    for (uint32_t i = 0; i < tensor_size; i++) {
+        index[0] = i;
+        EXPECT_EQ((uint32_t)t3[index], 3);
+    }
+}
+
+TEST(BasicTensorOperations, SubTensor) {
+    uint32_t tensor_size = 3;
+    Tensor t1({tensor_size}, Tensor::Type::INT32);
+    Tensor t2({tensor_size}, Tensor::Type::INT32);
+    t1 = 1;
+    t2 = 2;
+
+    auto t3 = t1 - t2;
+    std::vector<uint32_t> index(1, 0);
+
+    for (uint32_t i = 0; i < tensor_size; i++) {
+        index[0] = i;
+        EXPECT_EQ((int32_t)t3[index], -1);
+    }
+}
+
+TEST(BasicTensorOperations, MulTensor) {
+    uint32_t tensor_size = 3;
+    Tensor t1({tensor_size}, Tensor::Type::INT32);
+    Tensor t2({tensor_size}, Tensor::Type::INT32);
+    t1 = -1;
+    t2 = 2;
+
+    auto t3 = t1 * t2;
+    std::vector<uint32_t> index(1, 0);
+
+    for (uint32_t i = 0; i < tensor_size; i++) {
+        index[0] = i;
+        EXPECT_EQ((int32_t)t3[index], -2);
+    }
+}
+
+TEST(BasicTensorOperations, DivTensor) {
+    uint32_t tensor_size = 3;
+    Tensor t1({tensor_size}, Tensor::Type::INT32);
+    Tensor t2({tensor_size}, Tensor::Type::INT32);
+    t1 = -16;
+    t2 = -4;
+
+    auto t3 = t1 / t2;
+    std::vector<uint32_t> index(1, 0);
+
+    for (uint32_t i = 0; i < tensor_size; i++) {
+        index[0] = i;
+        EXPECT_EQ((int32_t)t3[index], 4);
     }
 }
